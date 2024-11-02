99c3f3fa3cb8569c580099a18cfb30b7991a4d77
TomZimny92 <tzimny92@gmail.com>
2024-11-01 20:34:09 -0400

    character counter

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def linearize(book):
-    chet = book.replace(" ", "")
 
 def wordCount(book):
     words = book.split()
@@ -12,18 +10,20 @@ def letterCount(book):
     lower_case_book = book.lower()
     linearized_book = lower_case_book.replace(" ", "")
     alphabetCount = {}
-    for letter in range(len(linearized_book)):
-        print(alphabetCount[letter])
-        if (ord(linearized_book[letter]) >= 97 or ord(linearized_book[letter]) <= 122):
-            alphabetCount[linearized_book[letter]] += 1
-            print(alphabetCount[letter])
-        
+    for i in range(len(linearized_book)):
+        letter = linearized_book[i]
+        if (ord(letter) >= 97 and ord(letter) <= 122):
+            if (letter not in alphabetCount):
+                alphabetCount[letter] = 1
+            else:
+                alphabetCount[letter] += 1
+    return alphabetCount
 
 def main():
     with open("books/frankenstein.txt") as frank:
         file_contents = frank.read()
         print(wordCount(file_contents))
-        letterCount(file_contents)
+        print(letterCount(file_contents))
 
 
 main()
